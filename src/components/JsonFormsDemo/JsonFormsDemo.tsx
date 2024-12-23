ad88ffaed691fc7df9d31dc58cdb27abdd595a9b
louis-stintzy <louis.stintzy@gmail.com>
2024-12-23 08:38:16 +0100

    refactor: extract form-building logic into a utility function

--- src/components/JsonFormsDemo/JsonFormsDemo.tsx
+++ src/components/JsonFormsDemo/JsonFormsDemo.tsx
@@ -1,14 +1,11 @@
 import { JsonForms } from "@jsonforms/react";
-// import { schema } from "../../schemas/Set1/schema";
-// import { UISchema } from "../../schemas/Set1/UISchema";
-// import { initialData } from "../../schemas/Set1/initialData";
 import { useMemo, useState } from "react";
 import { vanillaCells, vanillaRenderers } from "@jsonforms/vanilla-renderers";
 import BoundData from "./BoundData";
-import { allSets } from "../../schemas";
 
 import "./JsonFormsDemo.css";
 import { Layout } from "antd";
+import getInfoToBuildForm from "../../utils/getInfoToBuildForm";
 
 interface JsonFormsDemoProps {
   activeJSONForm: string[];
@@ -16,22 +13,7 @@ interface JsonFormsDemoProps {
 }
 
 function JsonFormsDemo({ activeJSONForm, showDrawer }: JsonFormsDemoProps) {
-  const [category, setRef] = activeJSONForm;
-  const activeSet = allSets.find((set) => set.ref === setRef);
-  if (!activeSet) {
-    throw new Error(`No set found for ref ${setRef}`);
-  }
-  const activeCategoryIndex = activeSet.uischema.elements.findIndex(
-    (element) => element.label === category
-  );
-
-  const schema = activeSet.schema;
-  const UISchemaElements = activeSet.uischema.elements[activeCategoryIndex];
-  const UISchema = {
-    type: "Categorization",
-    elements: [UISchemaElements],
-  };
-  const initialData = activeSet.initialData;
+  const { schema, UISchema, initialData } = getInfoToBuildForm(activeJSONForm);
 
   const [data, setData] = useState(initialData);
   const [resetKey, setResetKey] = useState(0);
